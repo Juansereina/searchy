dbd6fb753b094fa5d46597da02319bef6fc36eca
Juansereina <juansereina@outlook.com>
2021-02-28 19:26:01 -0500

    init

--- src/components/Logo/index.js
+++
@@ -1,5 +0,0 @@
-import './style.css';
-
-const Logo = () => <h1 className="logo">Searchy</h1>;
-
-export default Logo;
--- src/utils/index.js
+++
@@ -1,6 +0,0 @@
-/* General utilities */
-export const isString = value => typeof value === 'string';
-export const isArray = value =>  Array.isArray(value);
-export const isNumber = value => typeof value === 'number';
-export const isObject = value => value === Object(value);
-export const isLast = (arr, index) => arr.length -1 === index;
